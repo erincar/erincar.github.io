6c6a86383f8129c2338ff41243880b7372ddd24c
Erinc Argimak <erar93@gmail.com>
2021-05-19 23:53:07 +0200

    Add portfolio child elements

---
+++ src/components/Gallery.svelte
@@ -0,0 +1,15 @@
+<script></script>
+
+<style>
+    .gallery-container {
+    /* Placement */
+    @apply w-full h-full;
+    /* Layout */
+    /* Appearance */
+    @apply bg-green text-dark;
+  }
+</style>
+
+<div class="gallery-container">
+    <h1>Gallery</h1>
+</div>
---
+++ src/components/PersonCard.svelte
@@ -0,0 +1,15 @@
+<script></script>
+
+<style>
+  .personcard-container {
+    /* Placement */
+    @apply w-full h-full;
+    /* Layout */
+    /* Appearance */
+    @apply bg-dark text-light;
+  }
+</style>
+
+<div class="personcard-container">
+  <h1>PersonCard</h1>
+</div>
