ce51afc49a55f7dd8ada15f3562456cd72b2aa32
Erinç Argımak <erar93@gmail.com>
2023-12-03 00:27:39 +0100

    Update packages, Add threlte support

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,11 @@
+<script>
+	import { Canvas } from '@threlte/core';
+	import Scene from '$components/Scene.svelte';
+</script>
+
 <h1>yes, erinc.space.</h1>
+<div class="w-full">
+	<Canvas>
+		<Scene />
+	</Canvas>
+</div>
