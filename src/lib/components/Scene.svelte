425e84565a08628f3413455eee8b0187c038eecb
Erinç Argımak <erar93@gmail.com>
2025-05-11 22:05:54 +0200

    old WIP

--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -1,27 +1,43 @@
 <script>
 	import { T } from '@threlte/core';
 
-	import { interactivity } from '@threlte/extras';
+	import { interactivity, useGltf } from '@threlte/extras';
 	import { spring } from 'svelte/motion';
+	import Woven from './Woven.svelte';
 
 	interactivity();
 	const scale = spring(1);
+    export let letter = 't';
+
+    // Create LogoLetter.svelte
+    //   Loads the gltf with each letter (lower + upper) as a node
+    //   Loads the material through gltf (texture, normalMap)
+    //   Maybe loads lighting for it
+    //   Instantiated with prop letter, chooses that node to show
+    // Create PageLogo.svelte instead of this
+    //   Creates the logo scene and places LogoLetter components
+    //   Aligns them
+    //   Controls rotation of the objects, inferring from scroll location
+    // In the page layout, place the canvas into the middle
+    // Once the logo is scrolled out of view, it switches to the sticky top bar,
+    //   shown as the page logo on top left
 </script>
 
 <T.PerspectiveCamera
 	makeDefault
-	position={[2, 2, 2]}
+	position={[0.3,-0.7,0]}
 	on:create={({ ref }) => {
-		ref.lookAt(0, 1, 0);
+		ref.lookAt(0, 0, -1);
 	}}
 />
 
-<T.Mesh
-	position.y={1}
-	scale={$scale}
-	on:pointerenter={() => scale.set(1.5)}
-	on:pointerleave={() => scale.set(1)}
->
-	<T.BoxGeometry />
-	<T.MeshBasicMaterial color="red" />
-</T.Mesh>
+<T.DirectionalLight position={[0, 10, 10]} />
+
+
+<Woven
+  position={[0,0,-3]}
+  scale={2}
+  letter={letter}
+/>
+
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,19 @@
 <script>
 	import { Canvas } from '@threlte/core';
 	import Scene from '$components/Scene.svelte';
+
+    let letter = 't';
 </script>
 
 <h1>yes, erinc.space.</h1>
-<div class="w-full">
+<div class="w-full" style="height: 600px;">
 	<Canvas>
-		<Scene />
+		<Scene letter={letter}/>
 	</Canvas>
 </div>
+
+<select bind:value={letter} class="absolute bottom-1/2 right-0">
+    <option>e</option>
+    <option>x</option>
+    <option>t</option>
+</select>
