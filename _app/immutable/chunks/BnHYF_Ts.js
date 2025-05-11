4d548870c5254a20aaa83ad9a50853bfb702e24c
erincar <erincar@users.noreply.github.com>
2025-05-11 20:51:48 +0000

    deploy: 80726283d5024166893f6b671067fd70942b10a3

---
+++ _app/immutable/chunks/BnHYF_Ts.js
@@ -0,0 +1,1 @@
+import{e}from"./YMNxy12_.js";e();
---
+++ _app/immutable/nodes/2.CO2ACeUD.js
@@ -0,0 +1,1 @@
+import{t as p,a as t}from"../chunks/QIV4Hk93.js";import"../chunks/BnHYF_Ts.js";var e=p("<h1>erinc.space</h1>");function m(a){var o=e();t(a,o)}export{m as component};
